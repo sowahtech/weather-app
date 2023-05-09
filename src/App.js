c6efc069a3e441557165b3f35674c12ed57598b6
codafly2 <codittech@outlook.com>
2023-05-09 21:57:00 +0000

    app-left and app-right divs created, form, label and text box also added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="app-left">
+        <form>
+          <label>Your City</label>
+          <input type="submit" value="submit" placeholder="London" />
+        </form>
+      </div>
+      <div className="app-right"></div>
+    </div>
+  );
 }
 
 export default App;
