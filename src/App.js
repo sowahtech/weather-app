bb59da4bf60df25090a32c931f148ce89977ea06
codafly2 <codittech@outlook.com>
2023-05-09 22:52:02 +0000

    basic styling for outer-app-container and inner-app-container done

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="app-container">
-        <div className="app-left">
-          <form>
-            <label>Your City</label>
-            <input type="text" name="city" placeholder="London" />
-          </form>
+      <div className="outer-app-container">
+        <div className="inner-app-container">
+          <div className="app-left">
+            <form>
+              <label>Your City</label>
+              <input type="text" name="city" placeholder="London" />
+            </form>
+          </div>
+          <div className="app-right"></div>
         </div>
-        <div className="app-right"></div>
       </div>
     </div>
   );
